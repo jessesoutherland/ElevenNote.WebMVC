759e8c436b5794d5e029e69aedbcee7c3a854169
Jesse Southerland <jessemsoutherland@gmail.com>
2021-11-10 16:15:09 -0500

    navbar

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2), MaxLength(100)]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
